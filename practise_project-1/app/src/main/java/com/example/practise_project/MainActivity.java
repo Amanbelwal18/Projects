e295c956cfd2295089c9ddb1e433cacb2b63964f
Amanbelwal18 <143030875+Amanbelwal18@users.noreply.github.com>
2025-02-06 19:40:02 +0530

    Initial commit

---
+++ practise_project-1/app/src/main/java/com/example/practise_project/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.practise_project;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "GOOD MORNING!!!!!!!", Toast.LENGTH_SHORT).show();
+        text = findViewById(R.id.text);
+        text.setText("good morning!!!");
+
+
+
+
+
+
+    }
+}
